5cee89d544616ff8ff8be49f8b4d163c1cd028fa
Harshad <mahajan.harshad@gmail.com>
2016-04-08 12:49:22 +0530

    getting 405 error

--- src/main/java/vkicl/action/PortInwardDetailsJsonServlet.java
+++ src/main/java/vkicl/action/PortInwardDetailsJsonServlet.java
@@ -1,9 +1,7 @@
 package vkicl.action;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import vkicl.services.PortInwardDetailsJsonService;
-import vkicl.util.JqGridParametersHolder;
 
 public class PortInwardDetailsJsonServlet extends HttpServlet {
 
@@ -24,26 +21,52 @@ public class PortInwardDetailsJsonServlet extends HttpServlet {
 
 	private Logger logger = Logger.getLogger(PortInwardDetailsJsonServlet.class);
 
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		try {
+//			super.doGet(req, resp);
+//			PortInwardDetailsJsonService service = new PortInwardDetailsJsonService();
+//			String json = service.getPortInwardListAsJson(req);
+//
+//			logger.debug("Going to return PortInwardDetailsJsonServlet json ");
+//			logger.debug(json);
+//			// TODO: Chck if user is logged in (will implement it later)
+//			// TODO: check if user has required access (will implement it later)
+//
+//			resp.setContentType("application/json");
+//			resp.getWriter().write(json);
+//			resp.flushBuffer();
+//			resp.getOutputStream().flush();
+//			resp.getOutputStream().close();
+//
+//		} catch (SQLException e) {
+//			logger.error("Some eorror", e);
+//			throw new ServletException(e);
+//		}
+//	}
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.service(request, response);
 		try {
-			super.doGet(req, resp);
 			PortInwardDetailsJsonService service = new PortInwardDetailsJsonService();
-			String json = service.getPortInwardListAsJson(req);
+			String json = service.getPortInwardListAsJson(request);
 
 			logger.debug("Going to return PortInwardDetailsJsonServlet json ");
 			logger.debug(json);
-			// TODO: Chck if user is logged in (will implement it later)
-			// TODO: check if user has required access (will implement it later)
-			OutputStream out = resp.getOutputStream();
-			PrintWriter pw = new PrintWriter(out);
-			pw.write("{name:harshad}");
-			pw.flush();
-			pw.close();
-		} catch (SQLException e) {
-			logger.error("Some eorror", e);
+
+			response.setContentType("text/text;charset=utf-8");
+			response.setHeader("cache-control", "no-cache");
+			
+			PrintWriter out = response.getWriter();
+			out.println(json);
+			out.flush();
+			
+		} catch (Exception e) {
+			logger.error("Some error", e);
 			throw new ServletException(e);
 		}
 	}
-
+	
+	
 }
--- src/main/java/vkicl/services/PortInwardDetailsJsonService.java
+++ src/main/java/vkicl/services/PortInwardDetailsJsonService.java
@@ -1,18 +1,33 @@
 package vkicl.services;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.google.gson.Gson;
+
 import vkicl.daoImpl.PortDaoImpl;
+import vkicl.util.JqGridCustomResponse;
 import vkicl.util.JqGridParametersHolder;
 import vkicl.util.JqGridParametersHolder.JQGRID_PARAM_NAMES;
+import vkicl.util.JqGridSearchParameterHolder;
+import vkicl.vo.PortInwardRecordVO;
 
 public class PortInwardDetailsJsonService {
 	
-	public String getPortInwardListAsJson(HttpServletRequest req) throws SQLException{
-		String json = "";
+	private Logger logger = Logger.getLogger(PortInwardDetailsJsonService.class);
+	
+	public String getPortInwardListAsJson(HttpServletRequest req) throws SQLException, JsonParseException, JsonMappingException, IOException{
+		
 		JqGridParametersHolder params = new JqGridParametersHolder(req);
+		JqGridSearchParameterHolder searchParam = parseSerachFilters(params);
 		
 		String rows = params.getParam(JQGRID_PARAM_NAMES.rows);
 		String page = params.getParam(JQGRID_PARAM_NAMES.page);
@@ -21,10 +36,34 @@ public class PortInwardDetailsJsonService {
 		
 		
 		PortDaoImpl portDao = new PortDaoImpl();
-		Integer listSize = portDao.fetchPortInwardDetailsRecordCount();
-//		portDao.fetchPortInwardDetails_2(Integer.parseInt(page),
-//				Integer.parseInt(rows), listSize, orderBy, order, searchParam);
+		Integer totalRecordsCount = portDao.fetchPortInwardDetailsRecordCount();
+		List<PortInwardRecordVO> records = portDao.fetchPortInwardDetails_2(Integer.parseInt(page),
+				Integer.parseInt(rows), totalRecordsCount, orderBy, order, searchParam);
+		
+		JqGridCustomResponse response = new JqGridCustomResponse();
+		response.setPage(page);
+		response.setRows(records);
+		response.setRecords(rows);
+		response.setTotal(totalRecordsCount.toString());
+		Gson gson = new Gson();
+		String json = gson.toJson(response);
 		return json;
 	}
+	
+	
+	public JqGridSearchParameterHolder parseSerachFilters(JqGridParametersHolder params)
+			throws JsonParseException, JsonMappingException, IOException {
+		JqGridSearchParameterHolder searchParam = null;
+		String filters = params.getParam(JQGRID_PARAM_NAMES.filters);
+
+		if (filters != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			searchParam = mapper.readValue(filters,
+					JqGridSearchParameterHolder.class);
+			logger.info(searchParam);
+		}
+		return searchParam;
+	}
+	
 
 }
