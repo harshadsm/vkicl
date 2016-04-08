94717eb5d74e5214b32f8596a8aeb888a3e55458
Harshad <mahajan.harshad@gmail.com>
2016-04-08 12:51:21 +0530

    Fixed 405 Get method not supported error

--- src/main/java/vkicl/action/PortInwardDetailsJsonServlet.java
+++ src/main/java/vkicl/action/PortInwardDetailsJsonServlet.java
@@ -47,7 +47,7 @@ public class PortInwardDetailsJsonServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		super.service(request, response);
+		
 		try {
 			PortInwardDetailsJsonService service = new PortInwardDetailsJsonService();
 			String json = service.getPortInwardListAsJson(request);
