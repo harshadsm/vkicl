d72fae6b94dfe26723fee37555019181e7e0be0e
Ankush Darade <kush.darade@gmail.com>
2016-11-28 16:04:54 +0000

    fixed

--- src/main/java/vkicl/daoImpl/WarehouseShipmentDaoImpl.java
+++ src/main/java/vkicl/daoImpl/WarehouseShipmentDaoImpl.java
@@ -14,7 +14,7 @@ import vkicl.vo.PortOutwardPostDataContainerVO;
 import vkicl.vo.UserInfoVO;
 import vkicl.vo.WarehouseInwardRecordVO;
 
-public class WarehouseShipmentDaoImpl extends BaseDaoImpl{
+public class WarehouseShipmentDaoImpl extends BaseDaoImpl {
 
 	private Logger logger = Logger.getLogger(WarehouseShipmentDaoImpl.class);
 
@@ -23,71 +23,72 @@ public class WarehouseShipmentDaoImpl extends BaseDaoImpl{
 		Connection conn = null;
 		ResultSet rs = null;
 		CallableStatement cs = null;
-		
-		
+
 		try {
 			String query = "INSERT INTO warehouse_inward_shipment "
 					+ " (vehicle_number, received_date,vendor_name,create_ui,update_ui,create_ts,update_ts) "
 					+ " VALUES ( ?, ?, ?, ?, ?, ?, ?)";
-						
+
 			logger.info(query);
-			
+
 			conn = getConnection();
 			cs = conn.prepareCall(query);
-			
+
 			cs.setString(1, postDataContainer.getVehicleName());
-			
+
 			String vehicleDateStr = postDataContainer.getVehicleDate();
 			String vendorName = postDataContainer.getVendorName();
-			java.sql.Date vehicleDate = Converter.dateToSqlDate(Converter.stringToDate(vehicleDateStr, Constants.Apps.DATE_FORMAT));
-			
+			java.sql.Date vehicleDate = Converter
+					.dateToSqlDate(Converter.stringToDate(vehicleDateStr, Constants.Apps.DATE_FORMAT));
+
 			cs.setDate(2, vehicleDate);
 			cs.setString(3, vendorName);
 			cs.setString(4, userInfo.getUserName());
 			cs.setString(5, userInfo.getUserName());
 			cs.setString(6, getCurentTime());
 			cs.setString(7, getCurentTime());
-			
+
 			int count = cs.executeUpdate();
-			
+
 			ResultSet result = cs.getGeneratedKeys();
-			if(count > 0){
+			if (count > 0) {
 				result.next();
 				savedRecordId = result.getLong(1);
 			}
-			
+
 		} catch (Exception e) {
 			throw e;
-		}finally {
+		} finally {
 			closeDatabaseResources(conn, rs, cs);
 		}
 
 		return savedRecordId;
 	}
-	
-	public void updateWarehouseInwardFlag(WarehouseInwardRecordVO postDataContainer, UserInfoVO userInfo) throws SQLException {
+
+	public void updateWarehouseInwardFlag(WarehouseInwardRecordVO postDataContainer, UserInfoVO userInfo)
+			throws SQLException {
 		Connection conn = null;
 		ResultSet rs = null;
 		CallableStatement cs = null;
 		String query = "", message = "";
 		Integer savedRecordId = -1;
 		try {
-		
-			//java.sql.Date vehicleDate = Converter.dateToSqlDate(Converter.stringToDate(postDataContainer.getVehicleDate(), Constants.Apps.DATE_FORMAT));
-			
-			query = "UPDATE port_outward SET "
-					+ " warehouse_inward_flag =1 WHERE port_out_id="+postDataContainer.getPortInwardId();
-						
+
+			// java.sql.Date vehicleDate =
+			// Converter.dateToSqlDate(Converter.stringToDate(postDataContainer.getVehicleDate(),
+			// Constants.Apps.DATE_FORMAT));
+
+			query = "UPDATE port_outward SET " + " warehouse_inward_flag =1 WHERE port_out_id="
+					+ postDataContainer.getPortOutwardId();
+
 			logger.info(query);
-			
+
 			conn = getConnection();
 			cs = conn.prepareCall(query);
-			
-			
-			//cs.setLong(1, warehouseId);
-			
+
+			// cs.setLong(1, warehouseId);
+
 			savedRecordId = cs.executeUpdate();
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -96,8 +97,8 @@ public class WarehouseShipmentDaoImpl extends BaseDaoImpl{
 		} finally {
 			closeDatabaseResources(conn, rs, cs);
 		}
-		//return savedRecordId;
-		
+		// return savedRecordId;
+
 	}
 
 }
--- src/main/java/vkicl/vo/WarehouseInwardRecordVO.java
+++ src/main/java/vkicl/vo/WarehouseInwardRecordVO.java
@@ -6,132 +6,169 @@ public class WarehouseInwardRecordVO {
 	private Integer portInwardDetailId;
 	private Integer portInwardShipmentId;
 	private Integer portInwardId;
-	
+	private Integer portOutwardId;
+
+	public Integer getPortOutwardId() {
+		return portOutwardId;
+	}
+
+	public void setPortOutwardId(Integer portOutwardId) {
+		this.portOutwardId = portOutwardId;
+	}
+
 	private Integer length;
 	private Integer width;
 	private Double thickness;
 	private Integer availableQuantity;
-	
+
 	private String grade;
 	private String materialType;
 	private Double actualWt;
+
 	public Double getActualWt() {
 		return actualWt;
 	}
+
 	public void setActualWt(Double actualWt) {
 		this.actualWt = actualWt;
 	}
+
 	public String getActualWt_unit() {
 		return actualWt_unit;
 	}
+
 	public void setActualWt_unit(String actualWt_unit) {
 		this.actualWt_unit = actualWt_unit;
 	}
+
 	private String actualWt_unit;
-	
+
 	public String getMake() {
 		return make;
 	}
+
 	public void setMake(String make) {
 		this.make = make;
 	}
+
 	private String desc;
 	private String vesselDate;
 	private String vesselName;
 	private String make;
-	
+
 	public String getVendorName() {
 		return vendorName;
 	}
+
 	public void setVendorName(String vendorName) {
 		this.vendorName = vendorName;
 	}
+
 	private String vendorName;
-	
+
 	private String vehicleDate;
 	private String vehicleName;
 	private String millName;
-	
-	
+
 	private Double secWt;
 	private Double balQty;
-	
+
 	private String heatNo;
 	private String plateNo;
 	private String location;
-	
-	
-	
-	
+
 	public Double getSecWt() {
 		return secWt;
 	}
+
 	public void setSecWt(Double secWt) {
 		this.secWt = secWt;
 	}
+
 	public Double getBalQty() {
 		return balQty;
 	}
+
 	public void setBalQty(Double qty) {
 		this.balQty = qty;
 	}
+
 	public String getVehicleDate() {
 		return vehicleDate;
 	}
+
 	public void setVehicleDate(String vehicleDate) {
 		this.vehicleDate = vehicleDate;
 	}
+
 	public String getVehicleName() {
 		return vehicleName;
 	}
+
 	public void setVehicleName(String vehicleName) {
 		this.vehicleName = vehicleName;
 	}
+
 	public String getMillName() {
 		return millName;
 	}
+
 	public void setMillName(String millName) {
 		this.millName = millName;
 	}
+
 	public Integer getPortInwardDetailId() {
 		return portInwardDetailId;
 	}
+
 	public void setPortInwardDetailId(Integer portInwardDetailId) {
 		this.portInwardDetailId = portInwardDetailId;
 	}
+
 	public Integer getPortInwardShipmentId() {
 		return portInwardShipmentId;
 	}
+
 	public void setPortInwardShipmentId(Integer portInwardShipmentId) {
 		this.portInwardShipmentId = portInwardShipmentId;
 	}
+
 	public Integer getPortInwardId() {
 		return portInwardId;
 	}
+
 	public void setPortInwardId(Integer portInwardId) {
 		this.portInwardId = portInwardId;
 	}
+
 	public Integer getLength() {
 		return length;
 	}
+
 	public void setLength(Integer length) {
 		this.length = length;
 	}
+
 	public Integer getWidth() {
 		return width;
 	}
+
 	public void setWidth(Integer width) {
 		this.width = width;
 	}
+
 	public Double getThickness() {
 		return thickness;
 	}
+
 	public void setThickness(Double thickness) {
 		this.thickness = thickness;
 	}
+
 	public Integer getAvailableQuantity() {
 		return availableQuantity;
 	}
+
 	public void setAvailableQuantity(Integer availableQuantity) {
 		this.availableQuantity = availableQuantity;
 	}
@@ -139,66 +176,76 @@ public class WarehouseInwardRecordVO {
 	public String getGrade() {
 		return grade;
 	}
+
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
+
 	public String getDesc() {
 		return desc;
 	}
+
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
+
 	public String getVesselDate() {
 		return vesselDate;
 	}
+
 	public void setVesselDate(String vesselDate) {
 		this.vesselDate = vesselDate;
 	}
+
 	public String getVesselName() {
 		return vesselName;
 	}
+
 	public void setVesselName(String vesselName) {
 		this.vesselName = vesselName;
 	}
-	
-	
+
 	public String getMaterialType() {
 		return materialType;
 	}
+
 	public void setMaterialType(String materialType) {
 		this.materialType = materialType;
 	}
-	
-	
-	
+
 	public String getHeatNo() {
 		return heatNo;
 	}
+
 	public void setHeatNo(String heatNo) {
 		this.heatNo = heatNo;
 	}
+
 	public String getPlateNo() {
 		return plateNo;
 	}
+
 	public void setPlateNo(String plateNo) {
 		this.plateNo = plateNo;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	@Override
 	public String toString() {
 		return "WarehouseInwardRecordVO [portInwardDetailId=" + portInwardDetailId + ", portInwardShipmentId="
 				+ portInwardShipmentId + ", portInwardId=" + portInwardId + ", length=" + length + ", width=" + width
-				+ ", thickness=" + thickness + ", availableQuantity=" + availableQuantity 
-				+ ", grade=" + grade + ", materialType=" + materialType + ", desc=" + desc
-				+ ", vesselDate=" + vesselDate + ", vesselName=" + vesselName + ", vehicleDate=" + vehicleDate
-				+ ", vehicleName=" + vehicleName + ", millName=" + millName + ", secWt=" + secWt + ", balQty=" + balQty
-				+ ", heatNo=" + heatNo + ", plateNo=" + plateNo + ", location=" + location + "]";
+				+ ", thickness=" + thickness + ", availableQuantity=" + availableQuantity + ", grade=" + grade
+				+ ", materialType=" + materialType + ", desc=" + desc + ", vesselDate=" + vesselDate + ", vesselName="
+				+ vesselName + ", vehicleDate=" + vehicleDate + ", vehicleName=" + vehicleName + ", millName="
+				+ millName + ", secWt=" + secWt + ", balQty=" + balQty + ", heatNo=" + heatNo + ", plateNo=" + plateNo
+				+ ", location=" + location + "]";
 	}
-	
-	
+
 }
