44906340a90f7ac0267a3319708a3158c79cc8a5
HARSHAD MAHAJAN <mahajan.harshad@gmail.com>
2017-03-24 20:19:00 +0530

    Fixed search of port inward details

--- src/main/java/vkicl/daoImpl/PortDaoImpl.java
+++ src/main/java/vkicl/daoImpl/PortDaoImpl.java
@@ -333,8 +333,6 @@ public class PortDaoImpl extends BaseDaoImpl {
 		} else if (field != null && field.equalsIgnoreCase("vessel_name")) {
 			clause = "pis.vessel_name like '%" + data + "%'";
 		} else if (field != null && field.equalsIgnoreCase("vessel_date")) {
-			clause = "pis.vessel_date like '%" + data + "%'";
-		} else if (field != null && field.equalsIgnoreCase("vessel_date")) {
 			clause = processDateClause(data);
 		}
 
