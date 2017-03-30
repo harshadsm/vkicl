e6742f601d879ae7ce99b9fd1e7840b5b015e5c2
HARSHAD MAHAJAN <mahajan.harshad@gmail.com>
2017-03-30 21:40:21 +0530

    working on letting the user update actual weight of warehouse outward

---
+++ src/main/java/vkicl/daoImpl/WarehouseOutwardForActualWeightUpdateDaoImpl.java
@@ -0,0 +1,271 @@
+package vkicl.daoImpl;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import vkicl.util.Constants;
+import vkicl.util.Converter;
+import vkicl.util.JqGridSearchParameterHolder;
+import vkicl.util.JqGridSearchParameterHolder.Rule;
+import vkicl.vo.PortInwardRecordVO;
+import vkicl.vo.WarehouseOutwardReportVO;
+
+public class WarehouseOutwardForActualWeightUpdateDaoImpl extends BaseDaoImpl{
+	
+	private static Logger log = Logger.getLogger(WarehouseOutwardForActualWeightUpdateDaoImpl.class);
+
+	public Integer fetchWarehouseOutwardDetailsRecordCount(String orderByFieldName, String order, JqGridSearchParameterHolder searchParam) throws SQLException {
+		List<PortInwardRecordVO> list = new ArrayList<PortInwardRecordVO>();
+		Connection conn = null;
+		ResultSet rs = null;
+		CallableStatement cs = null;
+
+		int count = 0;
+		try {
+			conn = getConnection();
+//			String count_sql = " SELECT " + " count(*) " + " FROM ("
+//					+ "select * from warehouse_outward wo "
+//					+ processSearchCriteria(searchParam)
+//					+ ") a";
+			String count_sql = "select count(*) from ("+composeQueryForWarehouseOutwardDetails(orderByFieldName, order, searchParam)+" ) a";
+			log.info("query = " + count_sql);
+
+			cs = conn.prepareCall(count_sql);
+
+			rs = cs.executeQuery();
+			if (null != rs && rs.next()) {
+
+				do {
+					count = rs.getInt(1);
+
+					log.debug("Row count === " + count);
+				} while (rs.next());
+
+			}
+
+		} catch (Exception e) {
+			log.error("Some error", e);
+		} finally {
+			closeDatabaseResources(conn, rs, cs);
+		}
+		return count;
+	}
+	
+	private String processSearchCriteria(JqGridSearchParameterHolder searchParam) {
+		String sqlClause = "";
+		List<String> clauses = new ArrayList<String>();
+		if (null != searchParam && null != searchParam.getRules() && !searchParam.getRules().isEmpty()) {
+			for (JqGridSearchParameterHolder.Rule r : searchParam.getRules()) {
+				String clause = processSearchRule(r);
+				if (!StringUtils.isEmpty(clause)) {
+					clauses.add(clause);
+				}
+			}
+		}
+
+		// Prepare the sqlClause
+		sqlClause = prepareSqlClause(clauses);
+
+		return sqlClause;
+	}
+	
+	
+	
+	
+	
+
+	private String processSearchRule(Rule r) {
+		String data = r.getData();
+		String field = r.getField();
+		String op = r.getOp();
+
+		String clause = "";
+		if (field != null && field.equalsIgnoreCase("buyerName")) {
+			clause = "dispo.buyerName like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("vehicleNo")) {
+			clause = "wo.vehicle_no like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("vehicleDate")) {
+			
+			try {
+				String vehicleDate = stringToSqlDateString(data, Constants.Apps.DATE_FORMAT, Constants.Apps.DATE_FORMAT_SQL);
+				clause = "wo.vehicle_dt = date('" + vehicleDate + "')";
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				log.error("some error",e);
+			}
+			
+		} else if (field != null && field.equalsIgnoreCase("materialType")) {
+			clause = "sb.material_type like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("mill")) {
+			clause = "dd.millName like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("make")) {
+			clause = "dd.make like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("grade")) {
+			clause = "dd.grade like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("heatNo")) {
+			clause = "sb.heat_no like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("plateNo")) {
+			clause = "sb.plate_no like '%" + data + "%'";
+		} else if (field != null && field.equalsIgnoreCase("thickness")) {
+			clause = "dd.thickness = " + data;
+		}  else if (field != null && field.equalsIgnoreCase("length")) {
+			clause = "dd.length = " + data;
+		}  else if (field != null && field.equalsIgnoreCase("width")) {
+			clause = "dd.width = " + data;
+		} 
+
+		return clause;
+	}
+
+	
+	private String prepareSqlClause(List<String> clauses) {
+		String c = "";
+		int clausesCount = clauses.size();
+		for (int i = 0; i < clausesCount; i++) {
+			String s = clauses.get(i);
+			if (i > 0) {
+				s = " AND " + s;
+			}
+			c = c + " " + s;
+
+		}
+		if (!StringUtils.isEmpty(c)) {
+			c = " HAVING " + c;
+		}
+		return c;
+	}
+	
+	
+	private String processDateClause(String data) {
+		String clause = "";
+		SimpleDateFormat sdf = new SimpleDateFormat(Constants.Apps.DATE_FORMAT);
+		SimpleDateFormat sdfSql = new SimpleDateFormat(Constants.Apps.DATE_FORMAT_SQL);
+		try {
+			Date date = sdf.parse(data);
+			clause = "vessel_date = '" + sdfSql.format(date) + "'";
+
+		} catch (Exception e) {
+			log.error("some error", e);
+		}
+		return clause;
+	}
+	
+	
+	public List<WarehouseOutwardReportVO> fetchWarehouseOutwardDetails(int pageNo, int pageSize, long total,
+			String orderByFieldName, String order, JqGridSearchParameterHolder searchParam) throws SQLException {
+		List<WarehouseOutwardReportVO> list = new ArrayList<WarehouseOutwardReportVO>();
+		Connection conn = null;
+		ResultSet rs = null;
+		CallableStatement cs = null;
+		String query = "";
+
+		try {
+			conn = getConnection();
+
+			query = composeQueryForWarehouseOutwardDetails(orderByFieldName, order, searchParam);
+			log.info("query = " + query);
+
+			cs = conn.prepareCall(query);
+
+			rs = cs.executeQuery();
+			if (null != rs && rs.next()) {
+
+				do {
+					WarehouseOutwardReportVO vo = new WarehouseOutwardReportVO();
+					vo.setCreateTS(dateToString(rs.getDate(1), "dd-MM-yyyy"));
+					vo.setWarehouseOutwardId(rs.getInt(2));
+					vo.setDispatchNo(rs.getInt(3));
+					vo.setDispatchDetailId(rs.getInt(4));
+					vo.setVehicleNo(rs.getString(5));
+					vo.setVehicleDate(dateToString(rs.getDate(6), "dd-MM-yyyy"));
+					vo.setActualWeight(rs.getDouble(7));
+					vo.setMill(rs.getString(8));
+					vo.setMake(rs.getString(9));
+					vo.setGrade(rs.getString(10));
+					vo.setThickness(rs.getInt(11));
+					vo.setLength(rs.getInt(12));
+					vo.setWidth(rs.getInt(13));
+					vo.setOrderedQuantity(rs.getInt(14));
+					vo.setDeliveredQuantity(rs.getInt(15));
+					vo.setBuyerName(rs.getString(16));
+					vo.setMaterialType(rs.getString(17));
+					vo.setHeatNo(rs.getString(18));
+					vo.setPlateNo(rs.getString(19));
+					
+					list.add(vo);
+					
+				} while (rs.next());
+
+			}
+
+		} catch (Exception e) {
+			log.error("Some error", e);
+		} finally {
+			closeDatabaseResources(conn, rs, cs);
+		}
+		return list;
+	}
+
+	private String composeQueryForWarehouseOutwardDetails(String orderByFieldName, String order, JqGridSearchParameterHolder searchParam) {
+		String query;
+		StringBuffer q = new StringBuffer();
+		q.append(" select ");
+		q.append(" wo.create_ts warehouse_outward_creation_date,");
+		q.append(" wo.warehouse_outward_id,");
+		q.append(" wo.dispatchNo,");
+		q.append(" wo.dispatch_detail_id,");
+		q.append(" wo.vehicle_no,");
+		q.append(" wo.vehicle_dt,");
+		q.append(" wo.actual_wt,");
+		q.append(" ");
+		q.append(" dd.millName,");
+		q.append(" dd.make,");
+		q.append(" dd.grade,");
+		q.append(" dd.thickness,");
+		q.append(" dd.length,");
+		q.append(" dd.width,");
+		q.append(" dd.qty ordered_quantity,");
+		q.append(" wo.delivered_quantity,");
+		q.append(" dispo.buyerName,");
+		q.append(" sb.material_type, ");
+		q.append(" sb.heat_no, ");
+		q.append(" sb.plate_no ");
+		q.append("  from ");
+		q.append(" warehouse_outward wo");
+		q.append(" left join dispatch_details dd on wo.dispatch_detail_id = dd.dispatch_details_ID ");
+		q.append(" left join dispatch_order dispo on dd.dispatch_order_id = dispo.dispatch_order_id ");
+		q.append(" left join warehouse_outward_temp wot on wot.dispatch_details_id = wo.dispatch_detail_id ");
+		q.append(" left join stock_balance sb on sb.stock_balance_id = wot.stock_id ");
+		//q.append(" order by warehouse_outward_id desc;");
+		
+		//String sql = " select * from warehouse_outward "
+		String sql = q.toString()
+				+ processSearchCriteria(searchParam) + " " + composeOrderByClause(orderByFieldName, order) ;
+		query = sql;
+		return query;
+	}
+	
+	private String composeOrderByClause(String orderByFieldName, String order) {
+		String orderByClause = "";
+//		if (orderByFieldName != null) {
+//			orderByClause = " ORDER BY ";
+//			if (orderByFieldName.equalsIgnoreCase("vessel_date")) {
+//				orderByClause = orderByClause + " pis.vessel_date " + order + " ";
+//			} else if (orderByFieldName.equalsIgnoreCase("id")) {
+//				orderByClause = orderByClause + " pin.port_inward_id " + order + " ";
+//			}
+//		}
+		return orderByClause;
+	}
+}
---
+++ src/main/java/vkicl/services/WarehouseOutwardForActualWeightUpdateJsonService.java
@@ -0,0 +1,242 @@
+package vkicl.services;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.google.gson.Gson;
+
+import vkicl.daoImpl.PortDaoImpl;
+import vkicl.daoImpl.PortInwardOutwardIntersectionDaoImpl;
+import vkicl.daoImpl.PortInwardPackingListDaoImpl;
+import vkicl.daoImpl.PortOutwardDaoImpl;
+import vkicl.daoImpl.WarehouseOutwardDaoImpl;
+import vkicl.daoImpl.WarehouseOutwardForActualWeightUpdateDaoImpl;
+import vkicl.report.bean.PortOutwardBean2;
+import vkicl.util.JqGridCustomResponse;
+import vkicl.util.JqGridParametersHolder;
+import vkicl.util.JqGridParametersHolder.JQGRID_PARAM_NAMES;
+import vkicl.util.JqGridSearchParameterHolder;
+import vkicl.util.JqGridSearchParameterHolder.Rule;
+import vkicl.vo.PackingListItemVO;
+import vkicl.vo.PackingListItemVO2;
+import vkicl.vo.PortInwardRecordVO;
+import vkicl.vo.WarehouseOutwardReportVO;
+
+public class WarehouseOutwardForActualWeightUpdateJsonService {
+
+	private Logger logger = Logger.getLogger(WarehouseOutwardForActualWeightUpdateJsonService.class);
+
+	public String getWarehouseOutwardListAsJson(HttpServletRequest req)
+			throws SQLException, JsonParseException, JsonMappingException, IOException {
+
+		// String dd = req.getParameter("getPortInwardDetails");
+
+		JqGridParametersHolder params = new JqGridParametersHolder(req);
+		JqGridSearchParameterHolder searchParam = parseSerachFilters(params);
+
+		String rows = params.getParam(JQGRID_PARAM_NAMES.rows);
+		String page = params.getParam(JQGRID_PARAM_NAMES.page);
+		String orderBy = params.getParam(JQGRID_PARAM_NAMES.sidx);
+		String order = params.getParam(JQGRID_PARAM_NAMES.sord);
+
+		WarehouseOutwardForActualWeightUpdateDaoImpl warehouseOutwardDao = new WarehouseOutwardForActualWeightUpdateDaoImpl();
+		
+		Integer totalRecordsCount = warehouseOutwardDao.fetchWarehouseOutwardDetailsRecordCount(orderBy, order,searchParam);
+		List<WarehouseOutwardReportVO> records = warehouseOutwardDao.fetchWarehouseOutwardDetails(Integer.parseInt(page),
+				Integer.parseInt(rows), totalRecordsCount, orderBy, order, searchParam);
+
+		JqGridCustomResponse response = new JqGridCustomResponse();
+		response.setPage(page);
+		response.setRows(records);
+		response.setRecords(totalRecordsCount.toString());
+		response.setTotal((totalRecordsCount / Long.valueOf(rows)) + 1 + "");
+		Gson gson = new Gson();
+		String json = gson.toJson(response);
+		return json;
+	}
+
+	private JqGridSearchParameterHolder parseSerachFilters(JqGridParametersHolder params)
+			throws JsonParseException, JsonMappingException, IOException {
+		JqGridSearchParameterHolder searchParam = null;
+		String filters = params.getParam(JQGRID_PARAM_NAMES.filters);
+
+		if (filters != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			searchParam = mapper.readValue(filters, JqGridSearchParameterHolder.class);
+			logger.info(searchParam);
+		}
+		return searchParam;
+	}
+
+	private JqGridSearchParameterHolder parseSerachFilters2(JqGridParametersHolder params, HttpServletRequest request)
+			throws JsonParseException, JsonMappingException, IOException {
+		JqGridSearchParameterHolder searchParam = null;
+		String filters = params.getParam(JQGRID_PARAM_NAMES.filters);
+
+		if (filters != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			searchParam = mapper.readValue(filters, JqGridSearchParameterHolder.class);
+			logger.info(searchParam);
+		}
+
+		String portInwardIdStr = request.getParameter("portInwardId");
+		Rule portInwardIdRule = new Rule();
+		portInwardIdRule.setField("port_inward_id");
+		portInwardIdRule.setData(portInwardIdStr);
+		portInwardIdRule.setOp("eq");
+		if (searchParam == null) {
+			searchParam = new JqGridSearchParameterHolder();
+		}
+
+		if (searchParam.getRules() == null) {
+			searchParam.setRules(new ArrayList<Rule>());
+		}
+		searchParam.getRules().add(portInwardIdRule);
+		return searchParam;
+	}
+
+	public String getPackingListAsJson(HttpServletRequest req) throws Exception {
+
+		// String portInwardIdStr = req.getParameter("port_inward_database_id");
+		// logger.info("Port Inward Id = " + portInwardIdStr);
+		// Integer portInwardId = Integer.parseInt(portInwardIdStr);
+		// JqGridSearchParameterHolder.Rule portInwardIdRule = new
+		// JqGridSearchParameterHolder.Rule();
+		// portInwardIdRule.setField("port_inward_id");
+		// portInwardIdRule.setOp("eq");
+		// portInwardIdRule.setData(portInwardIdStr);
+
+		JqGridParametersHolder params = new JqGridParametersHolder(req);
+		JqGridSearchParameterHolder searchParam = parseSerachFilters(params);
+
+		if (searchParam == null) {
+			searchParam = new JqGridSearchParameterHolder();
+		}
+		if (searchParam.getRules() == null) {
+			List<JqGridSearchParameterHolder.Rule> rules = new ArrayList<JqGridSearchParameterHolder.Rule>();
+			searchParam.setRules(rules);
+		}
+		// searchParam.getRules().add(portInwardIdRule);
+
+		String rows = params.getParam(JQGRID_PARAM_NAMES.rows);
+		String page = params.getParam(JQGRID_PARAM_NAMES.page);
+		String orderBy = params.getParam(JQGRID_PARAM_NAMES.sidx);
+		String order = params.getParam(JQGRID_PARAM_NAMES.sord);
+
+		PortInwardPackingListDaoImpl portDao = new PortInwardPackingListDaoImpl();
+		Integer totalRecordsCount = portDao.fetchPortInwardPackingListRecordCount(searchParam);// ,
+																								// portInwardId);
+		List<PackingListItemVO> records = portDao.fetchPortInwardPackingList(Integer.parseInt(page),
+				Integer.parseInt(rows), totalRecordsCount, orderBy, order, searchParam);
+
+		records = updateAlreadyOutQuantity(records);
+
+		JqGridCustomResponse response = new JqGridCustomResponse();
+		response.setPage(page);
+		response.setRows(records);
+		response.setRecords(totalRecordsCount.toString());
+		response.setTotal((totalRecordsCount / Long.valueOf(rows)) + 1 + "");
+		Gson gson = new Gson();
+		String json = gson.toJson(response);
+		return json;
+	}
+
+	private List<PackingListItemVO> updateAlreadyOutQuantity(List<PackingListItemVO> records) throws Exception {
+		List<PackingListItemVO> recordsWithQtyMoreThanZero = new ArrayList<PackingListItemVO>();
+		PortOutwardDaoImpl portOutDaoImpl = new PortOutwardDaoImpl();
+
+		if (records != null && !records.isEmpty()) {
+			for (PackingListItemVO vo : records) {
+				// Find All Port Outward records for the given
+				// PortInwardDetailsId
+				Integer portInwardDetailId = vo.getPortInwardDetailId();
+				List<PortOutwardBean2> list = portOutDaoImpl.getByPortInwardDetailId(portInwardDetailId);
+
+				if (list != null && !list.isEmpty()) {
+					int sum = 0;
+					// sum the already out quantity
+					for (PortOutwardBean2 b : list) {
+						sum = sum + b.getQuantity();
+					}
+					// set it in the vo.
+					vo.setQuantityAlreadyOut(sum);
+					int portInwardQuantity = vo.getQuantity();
+					// int availableQuantity = portInwardQuantity - sum;
+					// vo.setQuantity(availableQuantity);
+					double balQty = (((vo.getLength() * vo.getWidth() * vo.getThickness() * portInwardQuantity * 7.85)
+							/ 1000000000));
+
+					double output = ((double) ((int) (balQty * 1000.0))) / 1000.0;
+
+					vo.setBalQty(output);
+
+				}
+
+				if (vo.getQuantity() != null && vo.getQuantity() > 0) {
+					recordsWithQtyMoreThanZero.add(vo);
+				}
+
+			}
+
+		}
+		return recordsWithQtyMoreThanZero;
+
+	}
+
+	public String getPackingListAsJson2(HttpServletRequest req) throws Exception {
+
+		// String portInwardIdStr = req.getParameter("port_inward_database_id");
+		// logger.info("Port Inward Id = " + portInwardIdStr);
+		// Integer portInwardId = Integer.parseInt(portInwardIdStr);
+		// JqGridSearchParameterHolder.Rule portInwardIdRule = new
+		// JqGridSearchParameterHolder.Rule();
+		// portInwardIdRule.setField("port_inward_id");
+		// portInwardIdRule.setOp("eq");
+		// portInwardIdRule.setData(portInwardIdStr);
+
+		JqGridParametersHolder params = new JqGridParametersHolder(req);
+		JqGridSearchParameterHolder searchParam = parseSerachFilters2(params, req);
+
+		if (searchParam == null) {
+			searchParam = new JqGridSearchParameterHolder();
+		}
+		if (searchParam.getRules() == null) {
+			List<JqGridSearchParameterHolder.Rule> rules = new ArrayList<JqGridSearchParameterHolder.Rule>();
+			searchParam.setRules(rules);
+		}
+		// searchParam.getRules().add(portInwardIdRule);
+
+		String rows = params.getParam(JQGRID_PARAM_NAMES.rows);
+		String page = params.getParam(JQGRID_PARAM_NAMES.page);
+		String orderBy = params.getParam(JQGRID_PARAM_NAMES.sidx);
+		String order = params.getParam(JQGRID_PARAM_NAMES.sord);
+
+		String portInwardIdStr = req.getParameter("portInwardId");
+		PortInwardPackingListDaoImpl portDao = new PortInwardPackingListDaoImpl();
+		Integer totalRecordsCount = portDao.fetchPortInwardPackingListRecordCount_2(searchParam, portInwardIdStr);// ,
+		// portInwardId);
+		List<PackingListItemVO2> records = portDao.fetchPortInwardPackingList_2(Integer.parseInt(page),
+				Integer.parseInt(rows), totalRecordsCount, orderBy, order, searchParam, portInwardIdStr);
+
+		// records = updateAlreadyOutQuantity(records);
+
+		JqGridCustomResponse response = new JqGridCustomResponse();
+		response.setPage(page);
+		response.setRows(records);
+		response.setRecords(totalRecordsCount.toString());
+		response.setTotal((totalRecordsCount / Long.valueOf(rows)) + 1 + "");
+		Gson gson = new Gson();
+		String json = gson.toJson(response);
+		return json;
+	}
+
+}
