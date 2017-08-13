b7d9bc348db1942feb4b55d44cf97ce2cb4b73e5
HARSHAD MAHAJAN <mahajan.harshad@gmail.com>
2017-08-13 20:13:47 +0530

    B1, A1: working on updating actual weight of delivery note line items

---
+++ src/main/java/vkicl/action/ActualWeightUpdateOfPortDeliveryNoteAction.java
@@ -0,0 +1,20 @@
+package vkicl.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import vkicl.util.Constants;
+
+public class ActualWeightUpdateOfPortDeliveryNoteAction extends BaseAction{
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) {
+		
+		ActionForward actionForward = mapping.findForward(Constants.Mapping.SUCCESS);
+		return actionForward;
+	}
+
+}
